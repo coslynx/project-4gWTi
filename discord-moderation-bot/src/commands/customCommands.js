128fcfbc353c4307fac6e94a8804adb726d40b5b
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-03 15:11:32 +0530

    Generated: `discord-moderation-bot/src/commands/customCommands.js`

---
+++ discord-moderation-bot/src/commands/customCommands.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+module.exports = {
+  name: 'customCommands',
+  description: 'Set up custom commands for quick access to frequently used moderation actions',
+  execute(message, args) {
+    // Add your custom command logic here
+  }
+};
