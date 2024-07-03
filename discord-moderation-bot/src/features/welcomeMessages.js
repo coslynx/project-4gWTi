deecfefc6aefdc893d5f176dedf2318d1c898122
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-03 15:11:56 +0530

    Generated: `discord-moderation-bot/src/features/welcomeMessages.js`

---
+++ discord-moderation-bot/src/features/welcomeMessages.js
@@ -0,0 +1,11 @@
+const welcomeMessages = {
+    sendWelcomeMessage: (user) => {
+        // Logic to send welcome message to the user
+    },
+
+    customizeWelcomeMessage: (message) => {
+        // Logic to customize the welcome message
+    }
+};
+
+module.exports = welcomeMessages;
